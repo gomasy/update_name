64ec403dd8ad35c8ec97a05c96795d9c6031d000
Gomashio <ktomohiro365@gmail.com>
2014-02-25 11:15:23 +0900

    Fix regex pattern

--- main.rb
+++ main.rb
@@ -51,9 +51,9 @@ loop do
         @allowed_user = object
       when Twitter::Tweet
         tweet = CGI.unescapeHTML(object.text.gsub(/@|＠/, "(at)"))
-        if tweet =~ /^(?!.*RT).*\(at\)#{tokens["screen_name"]}\supdate_name\s(.+?)$/
+        if tweet =~ /^(?!RT).*\(at\)#{tokens["screen_name"]}\supdate_name\s(.+?)$/
           update_name(object, $1)
-        elsif tweet =~ /^(?!.*RT)(.+?)(?:\(\s*\(at\)#{tokens["screen_name"]}\s*\)|（\s*\(at\)#{tokens["screen_name"]}\s*）)$/
+        elsif tweet =~ /^(?!RT)(.+?)(?:\(\s*\(at\)#{tokens["screen_name"]}\s*\)|（\s*\(at\)#{tokens["screen_name"]}\s*）)$/
           update_name(object, $1)
         end
       end

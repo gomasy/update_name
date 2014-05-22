a750d4cb0f17be8c7b19490ad91b4b81b88cb153
gomashio <ktomohiro365@gmail.com>
2014-05-23 01:27:03 +0900

    Revert "Except retweet"
    This reverts commit f2c63b628fd6970d254a6f1c1ae4f566893c3668.

--- plugins/say.rb
+++ plugins/say.rb
@@ -6,8 +6,6 @@ register_callback(:tweet) do |obj|
   screen_name = @credentials.screen_name
   case CGI.unescapeHTML(obj.text)
   when /^.*@#{screen_name}\ssay\s(.+?)$/
-    tweet = $1
-    @rest.update(tweet)
-    puts "System -> said: #{tweet}"
+    @rest.update($1)
   end
 end

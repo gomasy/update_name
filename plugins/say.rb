e346af267c100b616e9ed7bd540a75a59debfa18
Gomasy <nyan@gomasy.jp>
2014-12-17 03:34:22 +0900

    Refactored code due to https://github.com/Gomasy/update_name/commit/3bfdbe99966c712a10b7b6335ee795d97cd53e9a

--- plugins/say.rb
+++ plugins/say.rb
@@ -2,7 +2,7 @@
 
 require "cgi"
 
-register_callback(:tweet) do |obj|
+on_event(:tweet) do |obj|
   case CGI.unescapeHTML(obj.text)
   when /^(?!RT).*@#{screen_name}\ssay\s(.+?)$/
     twitter.update($1.sub(/@|＠/, "@\u200b"))
--- plugins/unfav_notify.rb
+++ plugins/unfav_notify.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-register_callback(:event) do |obj|
+on_event(:event) do |obj|
   case obj.name
   when :unfavorite
     twitter.update(

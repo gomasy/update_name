f8a056719e1bb148986b2fc02ea75690666ccadc
Gomasy <nyan@gomasy.jp>
2015-01-12 10:01:34 +0900

    plugins/say.rb: support newline

--- plugins/say.rb
+++ plugins/say.rb
@@ -4,7 +4,7 @@ require "cgi"
 
 on_event(:tweet) do |obj|
   case CGI.unescapeHTML(obj.text)
-  when /^(?!RT).*@#{screen_name}\ssay\s(.+?)$/
+  when /^(?!RT).*@#{screen_name}\ssay\s((.|\n)+?)$/
     twitter.update($1.sub(/@|＠/, "@\u200b"))
   end
 end

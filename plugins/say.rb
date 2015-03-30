7d318dd48751902295edb563cc7def45b5fc4822
Gomasy <nyan@gomasy.jp>
2015-03-30 15:01:30 +0900

    Refactored code

--- plugins/say.rb
+++ plugins/say.rb
@@ -2,7 +2,5 @@ require "cgi"
 
 on_event(:tweet) do |obj|
   ptn = /^(?!RT).*@#{screen_name}\ssay\s((.|\n)+?)$/
-  if CGI.unescapeHTML(obj.text) =~ ptn
-    twitter.update($1.sub(/@/, "@\u200b"))
-  end
+  twitter.update($1.sub(/@/, "@\u200b")) if CGI.unescapeHTML(obj.text) =~ ptn
 end

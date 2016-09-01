edf6d9cb090a01bba2b1609a79cc226fde1b95f5
Gomasy <nyan@gomasy.jp>
2016-09-01 14:23:13 +0900

    shell.rb: add timeout

--- plugins/shell.rb
+++ plugins/shell.rb
@@ -1,12 +1,13 @@
 require "cgi"
 
 def docker_run(cmd)
-  `docker run -it --rm base/archlinux /bin/sh -c "#{cmd.gsub(/"|'/, "\'")} 2>&1"`
+  `docker run -it --rm base/archlinux /bin/sh -c "timeout 1m #{cmd.gsub(/"|'/, "\'")} 2>&1"`
 end
 
 on_event(:tweet) do |obj|
   if CGI.unescapeHTML(obj.text) =~ /^(?!RT).*@#{screen_name}\sshell:\s?((.|\n)+?)$/
     r = docker_run($1)
+    log.info %(<bold>[System] Issued command: '#{$1}' by @#{obj.user.screen_name}</bold>)
 
     if "@#{obj.user.screen_name} #{r}".length > 140
       tw = "@#{obj.user.screen_name} ...#{r[obj.user.screen_name.length + r.length - 135..r.length]}"

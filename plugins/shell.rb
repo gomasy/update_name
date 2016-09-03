c67b642ec2bc99bb8a659726a5a1f43e241256b5
Gomasy <nyan@gomasy.jp>
2016-09-03 17:33:00 +0900

    shell.rb: refactored

--- plugins/shell.rb
+++ plugins/shell.rb
@@ -7,12 +7,13 @@ end
 on_event(:tweet) do |obj|
   if CGI.unescapeHTML(obj.text) =~ /^(?!RT).*@#{screen_name}\sshell:\s?((.|\n)+?)$/
     log.info %([System] Issued command: '#{$1}' by @#{obj.user.screen_name})
+    tw = "@#{obj.user.screen_name} "
     r = docker_run($1)
 
-    if "@#{obj.user.screen_name} #{r}".length > 140
-      tw = "@#{obj.user.screen_name} ...#{r[obj.user.screen_name.length + r.length - 135..r.length]}"
+    if tw.length + r.length > 140
+      tw << "...#{r[obj.user.screen_name.length + r.length - 135..r.length]}"
     else
-      tw = "@#{obj.user.screen_name} #{r}"
+      tw << r
     end
 
     twitter.update(tw, :in_reply_to_status_id => obj.id)
